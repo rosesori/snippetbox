604f786c5644109eff10143bfff1dea737a429ea
Rose Soriano <rosesoriano.dtx@gmail.com>
2024-12-22 18:39:48 -0600

    Add navigation template

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -19,6 +19,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 	files := []string{
 		"./ui/html/base.tmpl.html",
 		"./ui/html/pages/home.tmpl.html",
+		"./ui/html/partials/nav.gohtml",
 	}
 
 	/* Use the template.ParseFiles() function to read the files and store the
