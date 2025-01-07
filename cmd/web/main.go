59ccd9d00ece012bce84978e754eaba1a52d66b0
Rose Soriano <rosesoriano.dtx@gmail.com>
2025-01-06 23:36:31 -0800

    Incorporate sql.Open() in our application

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log/slog"
 	"net/http"
@@ -21,9 +22,10 @@ type application struct {
 func main() {
 	// Define command line flags
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
 
 	/* Use the flag.Parse() function to parse the command-line flag.
-	This reads in the command line flag value and assigns it to the addr variable.*/
+	This reads in the command line flag values and assign them to corresponding variables .*/
 	flag.Parse()
 
 	/* Initialize a new structured logger which writes to the standard out
@@ -31,6 +33,14 @@ func main() {
 	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
 		AddSource: true,
 	}))
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	// Defer so that the connection pool is closed before the main() function exits
+	defer db.Close()
 
 	/* Initialize a new instance of the application struct containing the
 	dependencies */
@@ -41,7 +51,25 @@ func main() {
 	logger.Info("starting server", "addr", *addr)
 
 	// We use the http.ListenAndServe() function to start a new web server.
-	err := http.ListenAndServe(*addr, app.routes())
+	err = http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+// The openDB() function wraps sql.Open() and returns a sql.DB connection pool for a given DSN.
+func openDB(dsn string) (*sql.DB, error) {
+	// Use sql.Open() to create a connection pool
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	// Use the Ping() method to check that the connection is working
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
