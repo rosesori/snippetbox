33711350993da3250b78e8f53c75dcf118d316bf
Rose Soriano <rosesoriano.dtx@gmail.com>
2025-01-15 23:17:29 -0800

    Fix typo when registering snippetView route handler

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -19,7 +19,7 @@ func (app *application) routes() *http.ServeMux {
 	/* Register the handler functions and corresponding route patterns with
 	the servemux */
 	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /snippert/view/{id}", app.snippetView)
+	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
 	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
 	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
 
